596cf1ff714e6c8e469d2bd96ab9e94e64bfb33c
Ben Anglin <ben.anglin@gmail.com>
2012-07-21 00:09:14 -0400

    Added capability to connect to wifi networks. Confirmed working with WPA and WPA2.

--- src/com/routerraiders/wifitester/WifiDetailActivity.java
+++ src/com/routerraiders/wifitester/WifiDetailActivity.java
@@ -5,13 +5,18 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiConfiguration.KeyMgmt;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class WifiDetailActivity extends Activity {
 
@@ -27,14 +32,13 @@ public class WifiDetailActivity extends Activity {
     private TextView mSecurity;
     private EditText mPassword;
     private EditText mNotes;
- 
+
     private Long mWifiId;
-    
 
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	this.setContentView(R.layout.wifi_details);
-	
+
 	mSsid = (TextView) this.findViewById(R.id.detail_ssid);
 	mBssid = (TextView) this.findViewById(R.id.detail_bssid);
 	mLocation = (TextView) this.findViewById(R.id.detail_location);
@@ -44,7 +48,7 @@ public class WifiDetailActivity extends Activity {
 	mSecurity = (TextView) this.findViewById(R.id.detail_security);
 	mPassword = (EditText) this.findViewById(R.id.detail_password);
 	mNotes = (EditText) this.findViewById(R.id.detail_notes);
-	
+
 	mWifiId = this.getIntent().getLongExtra(WIFI_ID_KEY, 0);
 
 	Log.d(TAG, "exiting onCreate");
@@ -84,17 +88,17 @@ public class WifiDetailActivity extends Activity {
 	SimpleDateFormat formatter = new SimpleDateFormat("MMMM d yyy 'at' hh:mm:ss");
 	String formattedDateString = formatter.format(date);
 	mLastSeen.setText(formattedDateString);
-	
+
 	mFrequency.setText(frequency.toString() + " MHz");
 	mSignal.setText(level.toString() + " dBm");
-	
+
 	mSecurity.setText(security);
 	mPassword.setText(password);
 	mNotes.setText(notes);
 
 	cursor.close();
 	database.close();
-	
+
 	Log.d(TAG, "exiting onStart");
     }
 
@@ -112,11 +116,70 @@ public class WifiDetailActivity extends Activity {
 		new String[] { mWifiId.toString() });
 
 	database.close();
-	
+
 	Log.d(TAG, "exiting onPause");
     }
-    
+
     public void onLoginClick(View button) {
+	
+	String ssid = mSsid.getText().toString();
+	String bssid = mBssid.getText().toString();
+	String security = mSecurity.getText().toString();
+	String password = mPassword.getText().toString();
+	
+	WifiConfiguration wifiConfig = generateWifiConfig(ssid, security, password);
+
+	WifiManager manager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+	Integer netId = manager.addNetwork(wifiConfig);
+	if (-1 != netId) {
+	    if (manager.enableNetwork(netId, true)) {
+		Toast.makeText(this.getApplicationContext(), R.string.login_successful, Toast.LENGTH_SHORT).show();
+		Log.i(TAG, "enabled network " + netId.toString());
+		manager.saveConfiguration();
+	    } else {
+		Toast.makeText(this.getApplicationContext(), R.string.login_failed, Toast.LENGTH_SHORT).show();
+	    }
+	} else {
+	    Toast.makeText(this.getApplicationContext(), R.string.wifi_add_failure, Toast.LENGTH_SHORT).show();
+	}
+	
+	for (WifiConfiguration config : manager.getConfiguredNetworks()) {
+	    Log.i(TAG, config.toString());
+	}
+
 	Log.d(TAG, "exiting onLoginClick");
+    }
+
+    private WifiConfiguration generateWifiConfig(String ssid, String security, String password) {
+	WifiConfiguration config = new WifiConfiguration();
+	config.SSID = "\"" + ssid + "\"";
+	config.status = WifiConfiguration.Status.ENABLED;
+	config.priority = 1;
+
+	if (security.contains("WPA")) {
+	    config.preSharedKey = "\"" + password + "\"";
+	    config.allowedKeyManagement.set(KeyMgmt.WPA_PSK);
+	} else if (security.contains("WEP")) {
+	    config.wepKeys[0] = password;
+	    config.wepTxKeyIndex = 0;
+	    config.allowedKeyManagement.set(KeyMgmt.NONE);
+	} else {
+	    config.allowedKeyManagement.set(KeyMgmt.NONE);
+	}
+
+	//config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+	//config.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+	//config.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+	//config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+	//config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+	//config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+	//config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+	//config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+	//config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+
+	Log.i(TAG, "Created configuration: " + config.SSID + " :: " + config.preSharedKey);
+	
+	
+	return config;
     }
 }
