c50549288141b924d3bd030b9fec048466dfd964
Ben Anglin <ben.anglin@gmail.com>
2012-10-08 15:43:05 -0400

    Fixed crash in versions <3.0

--- src/com/routerraiders/wifitester/LiveScanActivity.java
+++ src/com/routerraiders/wifitester/LiveScanActivity.java
@@ -1,6 +1,8 @@
 package com.routerraiders.wifitester;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,28 +10,31 @@ import android.support.v4.app.NavUtils;
 
 public class LiveScanActivity extends Activity {
 
+    @TargetApi(11)
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_live_scan);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_live_scan);
+	
+	if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_live_scan, menu);
-        return true;
+	getMenuInflater().inflate(R.menu.activity_live_scan, menu);
+	return true;
     }
 
-    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+	switch (item.getItemId()) {
+	case android.R.id.home:
+	    NavUtils.navigateUpFromSameTask(this);
+	    return true;
+	}
+	return super.onOptionsItemSelected(item);
     }
 
 }
