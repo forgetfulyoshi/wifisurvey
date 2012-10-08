f13c4c40dafd77225722131bc6ee5b39dad9d33c
Ben Anglin <ben.anglin@gmail.com>
2012-10-08 13:10:47 -0400

    Added a new homescreen - very basic at the moment

---
+++ src/com/routerraiders/wifitester/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.routerraiders.wifitester;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class HomeActivity extends Activity {
+
+    private static final String TAG = "HomeActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+    
+    public void onScanLogClicked(View v) {
+	Log.d(TAG, "onScanLogClicked");
+	Intent launch = new Intent(this, ScannerActivity.class);
+	startActivity(launch);
+    }
+}
