27c333d1a995cdc295804bc571569b87aa08cd22
Ben Anglin <ben.anglin@gmail.com>
2012-10-08 14:19:51 -0400

    Staring the scanner service

--- src/com/routerraiders/wifitester/HomeActivity.java
+++ src/com/routerraiders/wifitester/HomeActivity.java
@@ -15,6 +15,9 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        Intent scanServiceStart = new Intent(this, WifiScannerService.class);
+        startService(scanServiceStart);
     }
 
     @Override
