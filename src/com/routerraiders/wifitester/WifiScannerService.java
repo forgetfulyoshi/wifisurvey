67a7f59d86e1b5d88ed8a66cc0a909ce68aa6e9c
Ben Anglin <ben.anglin@gmail.com>
2012-10-08 14:19:24 -0400

    Fully implementing the scanner service

--- src/com/routerraiders/wifitester/WifiScannerService.java
+++ src/com/routerraiders/wifitester/WifiScannerService.java
@@ -8,55 +8,60 @@ import android.net.wifi.WifiManager;
 import android.util.Log;
 
 public class WifiScannerService extends IntentService {
-	private static final String TAG = "WifiScannerService";
+    private static final String TAG = "WifiScannerService";
 
-	private WifiManager mWifiService;
-	private WifiBroadcastReceiver mWifiReceiver;
-	private Boolean mOriginalWifiState;
+    private WifiManager mWifiService;
+    private WifiBroadcastReceiver mWifiReceiver;
+    private Boolean mOriginalWifiState;
+    private Boolean mRunning;
 
-	public WifiScannerService() {
-		super("WifiScannerService");
-		Log.d(TAG, "Constructed");
+    public WifiScannerService() {
+	super("WifiScannerService");
+	Log.d(TAG, "Constructed");
+    }
+
+    public void onCreate() {
+	super.onCreate();
+	mWifiService = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+
+	mOriginalWifiState = mWifiService.isWifiEnabled();
+
+	if (false == mOriginalWifiState) {
+	    mWifiService.setWifiEnabled(true);
 	}
 
-	public void onCreate() {
-		super.onCreate();
-		mWifiService = (WifiManager) this
-				.getSystemService(Context.WIFI_SERVICE);
-		
-		mOriginalWifiState = mWifiService.isWifiEnabled();
-		
-		if (false == mOriginalWifiState) {
-			mWifiService.setWifiEnabled(true);
+	mWifiReceiver = new WifiBroadcastReceiver();
+
+	registerReceiver(mWifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+	mRunning = true;
+
+	Log.d(TAG, "exiting onCreate");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+	// Start a wifi scan and sleep
+	Log.d(WifiScannerService.TAG, "entering onHandleIntent");
+
+	do {
+
+	    if (mWifiService.isWifiEnabled()) {
+		mWifiService.startScan();
+
+		try {
+		    Thread.sleep(10 * 1000);
+		} catch (InterruptedException e) {
+		    Log.e(TAG, "Could not sleep", e);
 		}
+	    }
+	} while (mRunning);
+    }
 
-		mWifiReceiver = new WifiBroadcastReceiver();
-
-		this.registerReceiver(mWifiReceiver, new IntentFilter(
-				WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-		Log.d(TAG, "exiting onCreate");
-	}
-
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		// Start a wifi scan and sleep
-		Log.d(WifiScannerService.TAG, "entering onHandleIntent");
-
-		if (mWifiService.isWifiEnabled()) {
-			mWifiService.startScan();
-
-			try {
-				Thread.sleep(10 * 1000);
-			} catch (InterruptedException e) {
-				Log.e(TAG, "Could not sleep", e);
-			}
-		}
-	}
-
-	public void onDestroy() {
-		super.onDestroy();
-		this.unregisterReceiver(mWifiReceiver);
-		mWifiService.setWifiEnabled(mOriginalWifiState);
-	}
+    public void onDestroy() {
+	super.onDestroy();
+	mRunning = false;
+	unregisterReceiver(mWifiReceiver);
+	mWifiService.setWifiEnabled(mOriginalWifiState);
+    }
 }
