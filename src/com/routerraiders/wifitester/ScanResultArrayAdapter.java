228a4d6883bef05fb015701c0682d131327914d3
Ben Anglin <ben.anglin@gmail.com>
2012-10-24 00:16:00 -0400

    Converting to use the new image handler.

--- src/com/routerraiders/wifitester/ScanResultArrayAdapter.java
+++ src/com/routerraiders/wifitester/ScanResultArrayAdapter.java
@@ -5,13 +5,14 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.routerraiders.wifitester.utils.ImageHandler;
 
 public class ScanResultArrayAdapter extends ArrayAdapter<ScanResult> {
 
@@ -42,30 +43,9 @@ public class ScanResultArrayAdapter extends ArrayAdapter<ScanResult> {
 	} else {
 	    holder = (ScanResultHolder) row.getTag();
 	}
-
-	Integer level = WifiManager.calculateSignalLevel(getItem(position).level, 5);
-
-	// TODO: There's probably a better way to do this
-	switch (level) {
-	case 0:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_0);
-	    break;
-	case 1:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_1);
-	    break;
-	case 2:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_2);
-	    break;
-	case 3:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_3);
-	    break;
-	case 4:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_4);
-	    break;
-	default:
-	    holder.wifiStrengthImg.setImageResource(R.drawable.wifi_strength_0);
-	    break;
-	}
+	
+	Integer iconId = ImageHandler.getWifiIconResource(mContext, getItem(position).level);
+	holder.wifiStrengthImg.setImageResource(iconId);
 
 	holder.bssidTxt.setText(getItem(position).BSSID);
 	holder.ssidTxt.setText(getItem(position).SSID);
