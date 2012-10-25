a493cae6adc501b7d982d439b9c03c6e4387ba22
Ben Anglin <ben.anglin@gmail.com>
2012-10-25 00:08:06 -0400

    Adding handling for locked image icons to address #15

--- src/com/routerraiders/wifitester/utils/ImageHandler.java
+++ src/com/routerraiders/wifitester/utils/ImageHandler.java
@@ -10,33 +10,58 @@ public class ImageHandler {
     public static Integer getWifiIconResource(Context context, int signalStrength) {
 	return getWifiIconResource(context, signalStrength, "");
     }
-    
+
     public static Integer getWifiIconResource(Context context, int signalStrength, String capabilities) {
 	Integer resourceId = R.drawable.wifi_strength_0;
+	Boolean encrypted = (capabilities.contains("WPA") || capabilities.contains("WEP"));
 
 	switch (WifiManager.calculateSignalLevel(signalStrength, 5)) {
 	case 0:
-	    resourceId = R.drawable.wifi_strength_0;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_0;
+	    } else {
+		resourceId = R.drawable.wifi_strength_0;
+	    }
 	    break;
 
 	case 1:
-	    resourceId = R.drawable.wifi_strength_1;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_1;
+	    } else {
+		resourceId = R.drawable.wifi_strength_1;
+	    }
 	    break;
 
 	case 2:
-	    resourceId = R.drawable.wifi_strength_2;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_2;
+	    } else {
+		resourceId = R.drawable.wifi_strength_2;
+	    }
 	    break;
 
 	case 3:
-	    resourceId = R.drawable.wifi_strength_3;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_3;
+	    } else {
+		resourceId = R.drawable.wifi_strength_3;
+	    }
 	    break;
 
 	case 4:
-	    resourceId = R.drawable.wifi_strength_4;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_4;
+	    } else {
+		resourceId = R.drawable.wifi_strength_4;
+	    }
 	    break;
-	    
+
 	default:
-	    resourceId = R.drawable.wifi_strength_0;
+	    if (encrypted) {
+		resourceId = R.drawable.wifi_strength_locked_0;
+	    } else {
+		resourceId = R.drawable.wifi_strength_0;
+	    }
 	    break;
 	}
 
