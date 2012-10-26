f0d28416eaa08bf766f11e7b81028b642d711cea
Ben Anglin <ben.anglin@gmail.com>
2012-10-25 22:00:27 -0400

    Adding the about screen. Fixes issue #13

--- src/com/routerraiders/wifitester/HomeActivity.java
+++ src/com/routerraiders/wifitester/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HomeActivity extends Activity {
@@ -13,28 +14,39 @@ public class HomeActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        
-        Intent scanServiceStart = new Intent(this, WifiScannerService.class);
-        startService(scanServiceStart);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_home);
+
+	Intent scanServiceStart = new Intent(this, WifiScannerService.class);
+	startService(scanServiceStart);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_home, menu);
-        return true;
+	getMenuInflater().inflate(R.menu.activity_home, menu);
+	return true;
     }
-    
+
     public void onScanLogClicked(View v) {
 	Log.d(TAG, "onScanLogClicked");
 	Intent launch = new Intent(this, ScanLogActivity.class);
 	startActivity(launch);
     }
-    
+
     public void onLiveScanClicked(View v) {
 	Log.d(TAG, "onLiveScanClicked");
 	Intent launch = new Intent(this, LiveScanActivity.class);
 	startActivity(launch);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+	switch (item.getItemId()) {
+	case R.id.menu_about:
+	    AboutDialog about = new AboutDialog(this);
+	    about.setTitle(R.string.menu_about);
+	    about.show();
+	    break;
+	}
+	return true;
     }
 }
